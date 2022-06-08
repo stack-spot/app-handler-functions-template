f899b6a93626be029c77a86c97faf44d818a5c17
StackSpot Admin <admin@stackspot.com>
2022-06-08 15:58:13 +0000

    Update public repository

--- templates/infra/bin/{{handler_name}}-bin.ts
+++ templates/infra/bin/{{handler_name}}-bin.ts
@@ -3,5 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { {{handler_name|title|replace("-", "")}}HandlerStack } from '../lib/{{handler_name}}-handler-stack';
 
+const {CDK_DEFAULT_ACCOUNT: account, CDK_DEFAULT_REGION: region} = process.env;
+
 const app = new cdk.App();
-new {{handler_name|title|replace("-", "")}}HandlerStack(app, '{{service_name|title|replace("-", "")}}HandlerStack');
+new {{handler_name|title|replace("-", "")}}HandlerStack(app, '{{service_name|title|replace("-", "")}}HandlerStack', {env: {account, region}});
